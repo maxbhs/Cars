006aed0b36b3a8c270e13939520823ffc925011d
kahino <woowuh@gmail.com>
2017-05-17 18:43:56 +0200

    turbos

--- Cars2/Assets/Scripts/Rotator.cs
+++ Cars2/Assets/Scripts/Rotator.cs
@@ -2,15 +2,37 @@
 using System.Collections;
 
 public class Rotator : MonoBehaviour {
+    public GameObject[] turbos;
+    public int contvalue = 200;
+    private int[] cont;
+    
 
 	// Use this for initialization
 	void Start () {
-	
+        cont = new int[turbos.Length];
+        for (int i = 0; i < cont.Length; ++i)
+        {
+            cont[i] = contvalue;
+        }
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+	void FixedUpdate () {
+        for (int i = 0; i < turbos.Length; ++i)
+        {
+            turbos[i].transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+            if(!turbos[i].activeSelf)
+            {
+                cont[i] = cont[i] - 1;
+                if (cont[i] == 0)
+                {
+                    turbos[i].SetActive(true);
+                    cont[i] = contvalue;
+                }
+            }
+        }
+        
+
 
     }
 }
