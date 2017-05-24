bb00c314516e7699f447eb86280646bbb157841c
kahino <woowuh@gmail.com>
2017-05-24 03:17:29 +0200

    partida terminada, esc para volver al menu

--- Cars2/Assets/Scripts/LevelManager.cs
+++ Cars2/Assets/Scripts/LevelManager.cs
@@ -1,13 +1,20 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
     public Text blueM;
     public Text orangeM;
+    public Text timer;
+    public Text ganador;
 
-    int blue, orange;
+    public float time;
+
+    private int blue, orange;
+    private string min, sec;
+    private bool finished;
 
     // Use this for initialization
     void Start () {
@@ -16,18 +23,70 @@ public class LevelManager : MonoBehaviour {
 
         blueM.text = blue.ToString();
         orangeM.text = orange.ToString();
+
+        finished = false;
+
+    }
+
+    void FixedUpdate ()
+    {
+        if(Input.GetKey(KeyCode.Escape)) SceneManager.LoadScene("Menu");
+        if (time <= 0)
+        {
+            finish();
+            return; //quizas a los profes no les mola esto jaja
+        }
+        time -= Time.deltaTime;
+        min = ((int)time / 60).ToString();
+        sec = (time % 60).ToString("f2");
+
+        if (time % 60 < 10)
+        {
+            timer.text = min + ":0" + sec;
+        } 
+        else timer.text = min + ":" + sec;
+        
     }
 	
 	public void blueGol ()
     {
-        ++blue;
-        blueM.text = blue.ToString();
+        if (!finished)
+        {
+            ++blue;
+            blueM.text = blue.ToString();
+        }
+        
     }
 
     public void orangeGol()
     {
-        ++orange;
-        orangeM.text = orange.ToString();
+        if (!finished)
+        {
+            ++orange;
+            orangeM.text = orange.ToString();
+        }
+        
+    }
+
+    private void finish()
+    {
+        finished = true;
+        timer.color = Color.yellow;
+        if (blue > orange)
+        {
+            ganador.text = "   BLUE TEAM WINS";
+            ganador.color = Color.blue;
+        }
+        else if (orange > blue)
+        {
+            ganador.text = "ORANGE TEAM WINS";
+        }
+        else
+        {
+            ganador.text = "             DRAW";
+            ganador.color = Color.black;
+        }
+
     }
 
 }
